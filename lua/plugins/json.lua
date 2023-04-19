0938685b09a7ce940b44efc5c105320d8a8b5a79
alex <aabinaei@gmail.com>
2023-04-19 02:30:06 -0700

    dropped elixir-tools, moved to lsp-config and other explicitly

---
+++ lua/plugins/json.lua
@@ -0,0 +1,2 @@
+-- add jsonls and schemastore ans setup treesitter for json, json5 and jsonc
+return { import = "lazyvim.plugins.extras.lang.json" }
---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,2 @@
+-- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+return { import = "lazyvim.plugins.extras.lang.typescript" }
