422e0b2c95a240cc28176fb133944169d67079c7
alex <aabinaei@gmail.com>
2023-04-18 23:19:54 -0700

    auto mix-format

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- elixir format on save
+vim.g.mix_format_on_save = 1
+vim.g.mix_format_silent_errors = 1
+vim.g.mix_format_options = "--check-equivalent"
