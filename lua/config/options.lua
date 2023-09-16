be06a2d3352f4c344c523ab0f8362313dcfb68e4
alex <aabinaei@gmail.com>
2023-09-16 10:04:29 -0700

    colorscheme config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.o.background = "light"
+
 -- elixir format on save
 vim.g.mix_format_on_save = 1
 vim.g.mix_format_silent_errors = 1
