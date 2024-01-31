07f6ee887da7098b27ae9dc1993b4dce198730e6
alex <aabinaei@gmail.com>
2024-01-31 11:04:41 -0800

    helm-ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,6 +73,23 @@ return {
           },
         },
       },
+      helm_ls = {
+        logLevel = "debug",
+        yamlls = {
+          enabled = true,
+          diagnosticsLimit = 50,
+          showDiagnosticsDirectly = false,
+          path = "yaml-language-server",
+          config = {
+            schemas = {
+              kubernetes = "**",
+            },
+            completion = true,
+            hover = true,
+            -- any other config: https://github.com/redhat-developer/yaml-language-server#language-server-settings
+          },
+        },
+      },
       html = {
         filetypes = {
           "html",
